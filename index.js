24afc9b74dc084be07e84ab72707ce7740fcb11d
kero8055 <32413241+kero8055@users.noreply.github.com>
2020-06-02 11:23:24 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Table from './App1';
 import ParentComponent from './App';
+import Cons from './Constructor';
 import * as serviceWorker from './serviceWorker';
 import App from './LifeCycle';
 
@@ -25,7 +26,30 @@ ReactDOM.render(<App />,document.getElementById('app')
 setTimeout(()=>{
   ReactDOM.unmountComponentAtNode(document.getElementById('app'));}, 10000);
 
+ReactDOM.render(<Cons />,document.getElementById('con'));
 
+/*let HelloReact= React.createClass({
+  getInitialState: function(){
+    return {
+      myMessage: 'I am from default state'
+    };
+  },
+  updateMyMessage: function(e){
+    this.setState({myMessage: e.target.value});
+  },
+  render:function() {
+    return(
+      <div>
+        <input type="text" onChange="{this.updateMyMessage}"></input>
+        <div>Hello React</div>
+        <div>
+          {this.state.myMessage}
+        </div>
+      </div>
+    );
+  }
+});
+ReactDOM.render(<HelloReact/>,document.getElementById('view'));*/
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
